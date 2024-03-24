6b97ab2c62d5d247785f9f016f982f9728fb4b06
alshakil83 <139704958+alshakil83@users.noreply.github.com>
2024-03-24 08:11:27 +0600

    y

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyPY89kbrcNeeolOCSLWbQZROKkkER3dXaxph7zXSzUU5Y-ukVJ5PIP3f_XnkqlHiN_gw/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
+      document.querySelector("form").addEventListener("submit", function(event) {
+          event.preventDefault();
+          
+          const input = document.getElementById("combinedInput").value;
+          
+          
+      });
+  
